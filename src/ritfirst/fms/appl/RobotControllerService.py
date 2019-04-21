6fd5b370d5859fd1388604f09ba867e61064ec17
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-04-20 23:58:31 -0400

    Implements fix for packet collisions

--- src/ritfirst/fms/appl/RobotControllerService.py
+++ src/ritfirst/fms/appl/RobotControllerService.py
@@ -26,6 +26,7 @@ class RobotControllerService:
         if not self.disabled:
             for i in range(len(self.dests)):
                 pack = Packet(PacketType.STATUS, RobotStateData.DISABLE)
+                print("Sent disable packet")
                 self.ntwk_mgnr.send_packet(pack, i)
         self.disabled = True
 
